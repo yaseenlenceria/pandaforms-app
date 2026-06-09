288d8827b3c3795dcec3f6eb7a67aa2f95815f4d
Yaseen <yaseen@example.com>
2026-06-09 22:24:39 +0500

    Fix App Bridge initialization: Move Meta component above CDN script tag in root.tsx

--- app/root.tsx
+++ app/root.tsx
@@ -12,13 +12,13 @@ export default function App() {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <Meta />
         <link rel="preconnect" href="https://cdn.shopify.com/" />
         <link
           rel="stylesheet"
           href="https://cdn.shopify.com/static/fonts/inter/v4/styles.css"
         />
         <script src="https://cdn.shopify.com/shopifycloud/app-bridge.js"></script>
-        <Meta />
         <Links />
       </head>
       <body>
